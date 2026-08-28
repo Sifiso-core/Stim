92b1b71d1ba2486a2a556afeb73f1abde3bd652f
Sifiso <Sifisodlamini123p@gmail.com>
2026-08-28 23:46:38 +0200

    feat(api): Initial Identity Role Data Seed

--- src/Stim.Api/Extensions/DatabaseExtensions.cs
+++ src/Stim.Api/Extensions/DatabaseExtensions.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Stim.Api.Data;
+using Stim.Api.Models.Common;
 
 namespace Stim.Api.Extensions;
 
@@ -9,16 +11,51 @@ public static class DatabaseExtensions
     {
         using IServiceScope scope = app.Services.CreateScope();
         await using ApplicationDbContext dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        await using ApplicationIdentityDbContext identityDbContext = scope.ServiceProvider.GetRequiredService<ApplicationIdentityDbContext>();
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<WebApplication>>();
 
         try
         {
             await dbContext.Database.MigrateAsync();
+            await identityDbContext.Database.MigrateAsync();
 
             app.Logger.LogInformation("Database migrations applied successfully.");
+
         }
         catch (Exception e)
         {
-            app.Logger.LogError(e, "An error occurred while applying database migrations.");
+            logger.LogError(e, "An error occurred while applying database migrations.");
+            throw;
+        }
+    }
+    public static async Task SeedInitialDataAsync(this WebApplication app)
+    {
+        using var scope = app.Services.CreateAsyncScope();
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<WebApplication>>();
+        var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+        string[] requiredRoles = [Roles.Member, Roles.Admin];
+        try
+        {
+            foreach (var roleName in requiredRoles)
+            {
+                if (!await roleManager.RoleExistsAsync(roleName))
+                {
+                    var result = await roleManager.CreateAsync(new IdentityRole(roleName));
+
+                    if (!result.Succeeded)
+                    {
+                        var errors = string.Join(", ", result.Errors.Select(e => e.Description));
+                        logger.LogError("Failed to create role '{Role}': {Errors}", roleName, errors);
+                    }
+                }
+            }
+
+            logger.LogInformation("Roles successfully seeded.");
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "An error occurred while seeding Identity roles.");
             throw;
         }
     }
--- src/Stim.Api/Program.cs
+++ src/Stim.Api/Program.cs
@@ -17,6 +17,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
     await app.ApplyMigrationsAsync();
+    await app.SeedInitialDataAsync();
 }
 
 app.UseHttpsRedirection();
